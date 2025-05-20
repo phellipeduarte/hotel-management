f064ed252248668baf936a08faf61c8a0b4ea5e7
phellipeduarte <phellipe.duarte@icloud.com>
2025-05-19 21:15:15 -0300

    Adicionando construtor privado para classe utils

--- src/main/Utils.java
+++ src/main/Utils.java
@@ -1,6 +1,9 @@
 import java.util.Locale;
 
 public class Utils {
+
+    private Utils(){}
+
     public static String getValueLocalCurrency(Double value){
         return "R$" + String.format(Locale.US, "%.2f", value);
     }
